75024b0e60d6aea8bf60c793d031ab869313a090
Mustafa <mustafazeinali@gmail.com>
2023-05-03 23:59:41 +0200

    header

---
+++ toys-and-joy/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import GetProduct from './components/url/Url'
+import Header from './components/header/Header'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <section>
+      <Header/>
+      <GetProduct/>
+    </section>
+  )
+}
+
+export default App
---
+++ toys-and-joy/src/components/header/Header.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components"
+
+const List = styled.ul`
+    text-decoration: none;
+    display : flex ;
+    flex-direction : row;
+    justify-content : flex-end;
+`
+const Item = styled.li`
+    margin : 2rem;
+    
+`
+
+const Header = () => {
+
+    return(
+        
+        <nav> 
+            <List> 
+                <Item> SHOP </Item>
+                <Item> SORT </Item>
+                <Item> LOG IN </Item>
+            </List>
+        </nav>
+       
+    )
+
+}
+
+export default Header
